2a65fa804c90b1ed3b93e767213dfa32eb514d9b
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-07-15 02:26:39 +0530

    Updates

---
+++ precache-manifest.cf093df6e1652e5ffec9c34ac9375e46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78fa90d82881b668209f45c6f62a4e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a54157c6a7674893a95",
+    "url": "/static/css/main.5cc3471b.chunk.css"
+  },
+  {
+    "revision": "ff3dd63586bb9caaae89",
+    "url": "/static/js/2.b1922fc5.chunk.js"
+  },
+  {
+    "revision": "6a54157c6a7674893a95",
+    "url": "/static/js/main.02732b73.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
