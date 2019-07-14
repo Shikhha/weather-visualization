f435db55885148d4e7dc6ba0559583790b733d85
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-07-15 02:30:55 +0530

    Updates

---
+++ precache-manifest.a2d9b09901b3c815f083b9509717e71f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ccb0eeecc94f815cb553b321d814965",
+    "url": "/weather-visualization/index.html"
+  },
+  {
+    "revision": "6a54157c6a7674893a95",
+    "url": "/weather-visualization/static/css/main.5cc3471b.chunk.css"
+  },
+  {
+    "revision": "ff3dd63586bb9caaae89",
+    "url": "/weather-visualization/static/js/2.b1922fc5.chunk.js"
+  },
+  {
+    "revision": "6a54157c6a7674893a95",
+    "url": "/weather-visualization/static/js/main.02732b73.chunk.js"
+  },
+  {
+    "revision": "0c81faea7f0aa4f5326e",
+    "url": "/weather-visualization/static/js/runtime~main.35bd24cc.js"
+  }
+]);
