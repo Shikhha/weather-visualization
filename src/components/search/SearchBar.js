9bb81d10c65faa45fcddfa6e145e203a4e6279ad
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-07-15 01:56:07 +0530

    added styled

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -17,19 +17,27 @@ class SearchBar extends React.Component {
   };
   render() {
     return (
-      <div className="container">
-        <form onSubmit={this.handleSubmit} className="form-inline">
-          <input
-            placeholder="Get a five day forecast in your favourite cities"
-            value={this.state.term}
-            onChange={this.handleChange}
-            className="form-control"
-            type="text"
-          />
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
+      <div className="container my-3">
+        <div className="border p-2">
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-row">
+              <div className="col-9 col-md-10">
+                <input
+                  placeholder="Get a five day forecast in your favourite cities....."
+                  value={this.state.term}
+                  onChange={this.handleChange}
+                  className="form-control"
+                  type="text"
+                />
+              </div>
+              <div className="col">
+                <button type="submit" className="btn btn-info">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
--- src/container/Weather_list.js
+++ src/container/Weather_list.js
@@ -48,17 +48,19 @@ class Weather_list extends Component {
   };
   render() {
     return (
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>City</th>
-            <th>Temperature (K)</th>
-            <th>Pressure (hPa)</th>
-            <th>Humidity (%)</th>
-          </tr>
-        </thead>
-        <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
-      </table>
+      <div className="custom-container">
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>City</th>
+              <th>Temperature (K)</th>
+              <th>Pressure (hPa)</th>
+              <th>Humidity (%)</th>
+            </tr>
+          </thead>
+          <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
+        </table>
+      </div>
     );
   }
 }
